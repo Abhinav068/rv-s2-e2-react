73f6eb14da98a1df41d2f349054718b2445216da
Abhinav068 <abhinav28897@gmail.com>
2023-08-17 22:31:11 +0530

    url updated

--- my-app/src/components/postdata.jsx
+++ my-app/src/components/postdata.jsx
@@ -2,30 +2,45 @@ import React, { useEffect, useState } from 'react'
 import RetrieveData from './retrieveData';
 
 const Postdata = () => {
-  const url = `abc`
+  const url = `https://drab-pink-dibbler-vest.cyclic.cloud`
+  // const url = `http://localhost:8000`
 
   const [allData, setAllData] = useState(['a']);
-
-  const datastyle = {
+  const [datastyle, setDatastyle] = useState({
     filter: null,
     order: 1
-  }
+  })
 
+  function fetchAllData() {
+    fetch(`${url}/api/travel`).then(response => response.json()).then(data => {
+      console.log(data.data);
+      setAllData([...data.data]);
+    });
+  }
 
   useEffect(() => {
-    console.log(allData);
+    handleDatastyle()
+  }, [datastyle])
 
-    // return () => {
-    //   second
-    // }
-  }, [])
 
-  function handleDatastyle(datastyle) {
-    console.log(datastyle);
+  function handleDatastyle() {
+    // console.log(datastyle);
+    fetch(`${url}/api/travel?dest=${datastyle.filter ? datastyle.filter : ''}&order=${datastyle.order}`).then(response => response.json()).then(data => {
+      console.log(data.data);
+      setAllData([...data.data])
+    });
   }
 
-  function handledelete(id){
+  function handledelete(id) {
     console.log(id);
+    fetch(`${url}/api/travel/${id}`, {
+      method: "DELETE",
+      headers: { 'Content-Type': 'application/json' }
+    }).then(response => response.json()).then(data => {
+      console.log(data);
+      alert(`${id} has been deleted.`)
+      fetchAllData();
+    });
   }
 
 
@@ -34,7 +49,7 @@ const Postdata = () => {
       <h1>Post your data</h1>
       <div>
         <form onSubmit={(event) => {
-          event.preventDefault();
+          // event.preventDefault();
           let obj = {
             name: event.target.name.value,
             email: event.target.email.value,
@@ -44,33 +59,44 @@ const Postdata = () => {
           };
           console.log(obj, url);
 
+          fetch(`${url}/api/travel`, {
+            method: "POST",
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(obj)
+          }).then(response => response.json()).then(data => {
+            console.log('response', data);
+            
+            fetchAllData();
+            
+          });
         }}>
-          <label htmlFor="name">Name</label> <input type="text" name="name" id="name" />
-          <label htmlFor="email">Email</label> <input type="text" name="email" id="email" />
-          <label htmlFor="destination">Destination</label> <select name="destination" id="destination">
+          <label htmlFor="name">Name</label> <input type="text" name="name" id="name" /> <br />
+          <label htmlFor="email">Email</label> <input type="text" name="email" id="email" /> <br />
+          <label htmlFor="destination">Destination</label> <select name="destination" id="destination"> <br />
             <option value="">Select your Destination</option>
             <option value="India">India</option>
             <option value="Africa">Africa</option>
             <option value="Europe">Europe</option>
             <option value="America">America</option>
-          </select>
-          <label htmlFor="travelers">No of Travelers</label> <input type="text" name="travelers" id="travelers" />
-          <label htmlFor="budget">Budget Per Person </label> <input type="text" name="budget" id="budget" />
-          <input type="submit" value="Post Data" />
+          </select> <br />
+          <label htmlFor="travelers">No of Travelers</label> <input type="text" name="travelers" id="travelers" /> <br />
+          <label htmlFor="budget">Budget Per Person </label> <input type="text" name="budget" id="budget" /> <br />
+          <input type="submit" value="Post Data" /> <br />
 
         </form>
       </div>
       <br /><br />
       <div>
         <button onClick={() => {
-          datastyle.order = (-1 * datastyle.order);
-          handleDatastyle(datastyle);
+          setDatastyle({ ...datastyle, order: -1 * datastyle.order })
+          
 
         }}>Sort by Budget Per Person</button>
         <label htmlFor="">Filter by Destination</label> <select onChange={(event) => {
           console.log(event.target.value);
-          datastyle.filter = event.target.value;
-          handleDatastyle(datastyle);
+          
+          setDatastyle({ ...datastyle, filter: event.target.value })
+          
 
         }}>
           <option value="">Select the Destination</option>
@@ -80,9 +106,7 @@ const Postdata = () => {
           <option value="America">America</option>
         </select>
       </div>
-      <RetrieveData alldata={allData} handledelete={handledelete}/>
-    
-     
+      <RetrieveData alldata={allData} handledelete={handledelete} />
 
     </div>
   )
--- my-app/src/components/retrieveData.jsx
+++ my-app/src/components/retrieveData.jsx
@@ -1,16 +1,26 @@
 import React from 'react'
 
-const RetrieveData = ({alldata}) => {
-  return (
-    <div>
-         {
-        alldata.map(el => (
-          <h2>{el}</h2>
-        ))
-      }
-    </div>
+const RetrieveData = ({ alldata, handledelete }) => {
+    return (
+        <div id='container'>
+            {
+                alldata.map(el => (
+                    <div>
 
-  )
+                        <p>Name: {el.name}</p>
+                        <p>Email: {el.email}</p>
+                        <p>Destination : {el.destination}</p>
+                        <p>No of Travellers : {el.no_of_travelers}</p>
+                        <p>Budget_Per_Person : {el.Budget_Per_Person}</p>
+                        <button onClick={() => {
+                            handledelete(el._id)
+                        }}>Delete</button>
+                    </div>
+                ))
+            }
+        </div>
+
+    )
 }
 
 export default RetrieveData
